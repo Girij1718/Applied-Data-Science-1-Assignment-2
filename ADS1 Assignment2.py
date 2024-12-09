c8b76cea48852a5658d50177d792c928828920e8
Girijprasad Janappa Gherade <girijjgherade1718@gmail.com>
2024-12-09 23:51:19 +0000

    File uploaded
    Inserted dataset

---
+++ ADS1 Assignment2.py
@@ -0,0 +1,8 @@
+import pandas as pd
+
+# Load the dataset
+file_path = r"C:\Users\girij\Downloads\Clustering\Adidas Vs Nike.csv"
+data = pd.read_csv(file_path)
+
+# Inspect the first few rows of the dataset
+data.head()
